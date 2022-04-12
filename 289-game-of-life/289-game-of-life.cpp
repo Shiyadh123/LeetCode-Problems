99e2ee2e31d6bd64b5113196da809675e7a3f654
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-04-12 13:13:27 +0530

    Time: 6 ms (17.56%), Space: 6.9 MB (84.44%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,31 +1,49 @@
 class Solution {
 public:
+    //notatn
+    // 0->1   2
+    // 1->0  -1
     int m,n;
+
     bool isValid(int i,int j){
         if(i<0||j<0||i>=m||j>=n) return false;
         return true;
     }
+
     void gameOfLife(vector<vector<int>>& board) {
         m=board.size();
         n=board[0].size();
+
         vector<int> dx={-1,0,1};
-        vector<vector<int>> temp(board);
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
+
                 int neighbourSum=0;
+
                 for(auto p:dx){
                     for(auto q:dx){
                         if(p!=0||q!=0){
-                            neighbourSum+=(isValid(i+p,j+q)?temp[i+p][j+q]:0);
+                            neighbourSum+=(isValid(i+p,j+q)?(board[i+p][j+q]==-1||board[i+p][j+q]==1?1:0):0);
                         }
                     }
                 }
-                if(neighbourSum<2) board[i][j]=0;
-                else if(neighbourSum==2)  board[i][j]=temp[i][j];
-                else if(neighbourSum==3) board[i][j]=1;
-                else board[i][j]=0;
+
+                if(neighbourSum<2) board[i][j]=board[i][j]==1?-1:0;
+                else if(neighbourSum==2)  board[i][j]=board[i][j];
+                else if(neighbourSum==3) board[i][j]=board[i][j]==0?2:1;
+                else board[i][j]=board[i][j]==1?-1:0;
             }
         }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==-1) board[i][j]=0;
+                if(board[i][j]==2) board[i][j]=1;
+
+            }
+        }
+
         return;
     }
 };
