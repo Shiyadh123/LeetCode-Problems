e733659e0a8863cdd7160dbffeb2561e416456d0
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-04-12 14:14:57 +0530

    Time: 841 ms (21.55%), Space: 9 MB (78.28%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int fun(vector<int>& nums,int i, int curr,int target){
+    int fun(vector<int>& nums,int i,int target){
         if(i==0){
-            return  (curr-nums[i]==target)+ (curr+nums[i]==target);
+            return  (nums[0]==target)+(nums[0]==-1*target);
         }
-        return fun(nums,i-1,curr-nums[i],target)+fun(nums,i-1,curr+nums[i],target);
+        return fun(nums,i-1,target-nums[i])+fun(nums,i-1,target+nums[i]);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        return fun(nums,n-1,0,target);
+        return fun(nums,n-1,target);
     }
 };
