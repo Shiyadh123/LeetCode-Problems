3a854c4207eb623c82eb4282c936fdd53f477a36
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-05-29 20:09:00 +0530

    Time: 19 ms (57.45%), Space: 13.2 MB (54.64%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,28 +3,38 @@ public:
     void setZeroes(vector<vector<int>>& a) {
          int m=a.size();
          int n=a[0].size();
-         bool flag=1;//row0col0 as col0 indicator
-        
+         bool row0=1,col0=1;
+
          for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 
                 if(a[i][j]==0){
-                    
-                    if(j==0) flag=0;
-                    else a[0][j]=0;
-                    
                     a[i][0]=0;
+                    a[0][j]=0;
+                    if(i==0) row0=0;
+                    if(j==0) col0=0;
                 }
+
             }
         }
         
-         for(int j=n-1;j>=0;j--){
-            for(int i=m-1;i>=0;i--){
+         for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 
-                if(a[i][0]==0||(j==0&&flag==0)||(j!=0&&a[0][j]==0)){
+                if(a[i][0]==0||a[0][j]==0){
                     a[i][j]=0;
                 }
-                
+
+            }
+        }
+        if(!col0){
+            for(int i=1;i<m;i++){
+                a[i][0]=0;
+            }
+        }
+        if(!row0){
+            for(int j=1;j<n;j++){
+                a[0][j]=0;
             }
         }
         return;
