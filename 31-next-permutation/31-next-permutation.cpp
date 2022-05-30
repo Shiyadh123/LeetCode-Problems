002be66190c299d889861703b3ed3bef9a53e02d
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-05-30 10:38:28 +0530

    Time: 9 ms (26.12%), Space: 12.2 MB (37.75%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -5,20 +5,18 @@ public:
          int ind=-1;
         
          if(n==0||n==1) return;
-        for(int i=n-1;i>=1;i--){
+         for(int i=n-1;i>=1;i--){
              if(nums[i-1]<nums[i]){
                 ind=i-1;
                  break;
              }
-        }
-       // cout<<nums[0]<<nums[1]<<nums[2]<<" ";
-         for(int j=n-1;j>=0&&ind!=-1;j--){
-                     if(nums[ind]<nums[j]){
-                         swap(nums[ind],nums[j]);
-                         break;
-                     }
-                 }
-// cout<<nums[0]<<nums[1]<<nums[2]<<" ";
+         }
+         for(int j=n-1;(j>=0&&ind!=-1);j--){
+             if(nums[ind]<nums[j]){
+                 swap(nums[ind],nums[j]);
+                 break;
+             }
+          }
         reverse(nums.begin()+ind+1,nums.end());
     }
 };
