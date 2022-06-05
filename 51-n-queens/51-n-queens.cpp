4a0926e9c806a7c8dfacee67ea22f720db4a71ca
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-06-05 10:01:42 +0530

    Time: 312 ms (5.00%), Space: 64.7 MB (5.31%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<string> board,int n){
-        for(int i=row-1;i>=0;i--){
-            if(board[i][col]=='Q') return false;
-        }
-        int i=row-1,j=col+1;
-        while(i>=0&&j<n){
-            if(board[i][j]=='Q') return false;
-            i--;
-            j++;
-        }
-        i=row-1,j=col-1;
-        while(i>=0&&j>=0){
-            if(board[i][j]=='Q') return false;
-            i--;
-            j--;
-        }
-        return true;
+
+    bool isSafe(int row,int col,vector<string> board,int n,vector<bool> top,vector<bool> rightDia,vector<bool> leftDia){
+       if(top[col]==1) return false;
+       if(rightDia[col+row]==1) return false;
+       if(leftDia[n+col-row]==1) return false;
+       return true;
     }
-    void fillRow(int i,int n,vector<string> board, vector<vector<string>> &ans){
-        // cout<<i<<" ";
-        // cout<<i<<" "<<board[i-1]<<endl;
+    void fillRow(int i,int n,vector<string> board, vector<vector<string>> &ans,vector<bool> top,vector<bool> rightDia,vector<bool> leftDia){
         if(i>=n) {ans.push_back(board); return;}
         for(int j=0;j<n;j++){
-            if(isSafe(i,j,board,n)){
+            if(isSafe(i,j,board,n,top,rightDia,leftDia)){
+                top[j]=1;
+                rightDia[i+j]=1;
+                leftDia[n+j-i]=1;
                 board[i][j]='Q';
-                fillRow(i+1,n,board,ans);
+                fillRow(i+1,n,board,ans,top,rightDia,leftDia);
                 board[i][j]='.';
+                top[j]=0;
+                rightDia[i+j]=0;
+                leftDia[n+j-i]=0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<string> board(n);
         vector<vector<string>> ans;
+        vector<bool> top(n,0);
+        vector<bool> rightDia(2*n+1,0);
+        vector<bool> leftDia(2*n+1,0);
         string s(n,'.');
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-        fillRow(0,n,board,ans);
+        fillRow(0,n,board,ans,top,rightDia,leftDia);
         return ans;
     }
 };
