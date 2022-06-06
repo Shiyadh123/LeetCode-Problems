29b38048ce8e54b394ceb351b3f86b73444db34e
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-06-06 18:24:40 +0530

    Time: 59 ms (57.46%), Space: 14.7 MB (31.40%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,25 +9,36 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        stack<ListNode*> st1;
-        stack<ListNode*> st2;
-        while(headA!=NULL) {
-            st1.push(headA);
-            headA=headA->next;
+        int l1=0,l2=0;
+        ListNode *t1=headA;
+        ListNode *t2=headB;
+        ListNode *u1=headA;
+        ListNode *u2=headB;
+        while(u1!=NULL) {
+            l1++;
+            u1=u1->next;
         }
-        while(headB!=NULL) {
-            st2.push(headB);
-            headB=headB->next;
+        while(u2!=NULL) {
+            l2++;
+            u2=u2->next;
         }
-        ListNode * tmp;
-        while(!st1.empty()&&!st2.empty()){
-            if(st1.top()!=st2.top()) return st1.top()->next;
-            tmp=st1.top();
-            st2.pop();
-            st1.pop();
+        if(l1>l2){
+            int k=l1-l2;
+            while(k--){
+                t1=t1->next;
+            }
         }
-        if(st1.empty()) return tmp;
-        if(st2.empty()) return tmp;
+        else if(l2>l1){
+            int k=l2-l1;
+            while(k--){
+                t2=t2->next;
+            }
+        }
+        while(t1!=NULL&&t2!=NULL){
+            if(t1==t2) return t1;
+            t1=t1->next;
+            t2=t2->next;
+        }
         return NULL;
     }
 };
