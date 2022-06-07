a4adb10dee9acf7a5bda97acdad5f1a94c506951
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-06-07 07:29:25 +0530

    Time: 3 ms (72.68%), Space: 9 MB (72.57%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    void reorder ( vector<int>& nums2){
-        int n=nums2.size();
-        if (n==1) return;
-        for(auto i=0;i<n-1;i++){
-            if(nums2[i]>nums2[i+1]) swap(nums2[i],nums2[i+1]);
-        }
-        return;
-    }
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(n==0) return;
-        int i=0,j=0;
-        while(i<m&&j<n){
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0&&j>=0){
             if(nums1[i]>nums2[j]){
-                swap(nums1[i],nums2[j]);
-                reorder(nums2);
+                nums1[k--]=nums1[i--];
+            }else{
+                nums1[k--]=nums2[j--];
             }
-            // for(auto it:nums1) cout<<it<<" ";
-            i++;
-            // cout<<endl<<i<<" "<<j<<endl;
         }
-        for(int p=0;p<n;p++){
-            nums1[p+m]=nums2[p];
+        while(i!=-1){
+            nums1[k--]=nums1[i--];
+        }
+        while(j!=-1){
+            nums1[k--]=nums2[j--];
         }
         return;
     }
