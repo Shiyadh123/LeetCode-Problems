a5388d1570340f2464416cda9a49ed99e1c4cae6
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-06-09 11:01:47 +0530

    Time: 17 ms (63.14%), Space: 15.7 MB (44.00%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -2,20 +2,28 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int n=numbers.size();
-        int i=0,j=n-1;
-        vector<int> ans;
-        while(i<j){
-            int sum=numbers[i]+numbers[j];
-            if(sum==target){
-                ans.push_back(i+1);
-                ans.push_back(j+1);
-                return ans;
-            }else if(sum>target){
-                j--;
-            }else{
-                i++;
+        // int i=0,j=n-1;
+        // while(i<j){
+        //     int sum=numbers[i]+numbers[j];
+        //     if(sum==target){
+        //         return {i+1,j+1};
+        //     }else if(sum>target){
+        //         j--;
+        //     }else{
+        //         i++;
+        //     }
+        // }
+        // return {};
+        for(int i=0;i<n;i++){
+            int toFind=target-numbers[i];
+            int lo=i+1,hi=n-1;
+            while(lo<=hi){
+                int mid=(lo+hi)/2;
+                if(numbers[mid]==toFind) return {i+1,mid+1};
+                else if(numbers[mid]>toFind) hi=mid-1;
+                else lo=mid+1;
             }
         }
-        return ans;
+        return {};
     }
 };
