7355a1314c65ff7995dde96da9d6d3f129f56868
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-06-10 11:09:30 +0530

    Time: 24 ms (55.28%), Space: 8.3 MB (59.36%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -6,21 +6,12 @@ public:
         int l=0,r=0;
         unordered_map<char,int> mp;
         while(r<n){
-             if(mp.find(s[r])==mp.end()){
-                mp[s[r]]=r;
-                maxLen=max(maxLen,r-l+1);
-             }else{
-                 if(mp[s[r]]>=l){
-                     l= mp[s[r]]+1;
-                     mp[s[r]]=r;
-                 }else{
-                     mp[s[r]]=r;
-                 }
-                 maxLen=max(maxLen,r-l+1);
-             }
-             r++;
+              if(mp.find(s[r])!=mp.end()) if(mp[s[r]]>=l) l=mp[s[r]]+1; 
+
+              mp[s[r]]=r;
+              maxLen=max(maxLen,r-l+1);
+              r++;
         }
         return maxLen;
     }
 };
- 
