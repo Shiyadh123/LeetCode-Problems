512e8d4d3992fa2461f08f73cbe3d5d5e4e11e87
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-06-12 20:07:12 +0530

    Time: 634 ms (100.00%), Space: 80.4 MB (100.00%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -3,26 +3,20 @@ public:
     int minPathCost(vector<vector<int>>& a, vector<vector<int>>& moveCost) {
         int m=a.size();
         int n=a[0].size();
-         vector<vector<int>> dp(m,vector<int> (n,0));
-        for(int i=0;i<n;i++){
-            dp[0][i]=a[0][i];
-        }
-         int mainMin=INT_MAX;
+        vector<vector<int>> dp(m,vector<int> (n,0));
+        
+    dp[0]=a[0];
+        
         for(int i=1;i<m;i++){
-            mainMin=INT_MAX;
             for(int j=0;j<n;j++){
                 int mini=INT_MAX;
                 for(int k=0;k<n;k++){
                     mini=min(mini,a[i][j]+dp[i-1][k]+moveCost[a[i-1][k]][j]);
                 }
                 dp[i][j]=mini;
-                mainMin=min(mainMin,dp[i][j]);
             }
         }
-        // for(auto it1:dp){
-        //     for(auto it2:it1) cout<<it2<<" ";
-        //     cout<<endl;
-        // }
-        return mainMin;
+         
+        return *min_element(dp[m-1].begin(),dp[m-1].end());
     }
 };
