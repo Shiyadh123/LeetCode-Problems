7f94d614c8589972902484afb72e1b48d20b5a04
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-06-13 07:19:41 +0530

    Time: 10 ms (43.76%), Space: 8.8 MB (55.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,15 +3,18 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         if(n==1) return triangle[0][0];
-        vector<vector<int>> dp(triangle);
+        vector<int> curr(n);
+        vector<int> prev(n);
+        prev[0]=triangle[0][0];
         for(int i=1;i<n;i++){
             for(int j=0;j<=i;j++){
                 int top=INT_MAX,left=INT_MAX;
-                if(j!=0) left=dp[i-1][j-1];
-                if(j!=i) top=dp[i-1][j];
-                dp[i][j]=min(top,left)+triangle[i][j];
+                if(j!=0) left=prev[j-1];
+                if(j!=i) top=prev[j];
+                curr[j]=min(top,left)+triangle[i][j];
             }
+            prev=curr;
         }
-        return *min_element(dp[n-1].begin(),dp[n-1].end());
+        return *min_element(curr.begin(),curr.end());
     }
 };
