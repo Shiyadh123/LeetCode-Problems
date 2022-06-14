f25f2a789c683128805ee2b5811517e5365e0c70
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-06-14 15:04:41 +0530

    Time: 282 ms (32.63%), Space: 83 MB (40.88%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -2,17 +2,27 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
          int n=nums.size();
-         bool npresent=false;
-         for(int i=0;i<n;i++){
-            if(nums[i]==n) npresent=true;
-            if(nums[i]<=0) nums[i]=n;
+        //  bool npresent=false;
+        //  for(int i=0;i<n;i++){
+        //     if(nums[i]==n) npresent=true;
+        //     if(nums[i]<=0) nums[i]=n;
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(abs(nums[i])<n) nums[abs(nums[i])]=-abs(nums[abs(nums[i])]);
+        // }
+        // for(int i=1;i<n;i++){
+        //     if(nums[i]>0) return i;
+        // }
+        // return npresent?n+1:n;
+
+        for(int i=0;i<n;i++){
+            while(nums[i]>0&&nums[i]<=n&&nums[i]!=nums[nums[i]-1]){
+                swap(nums[i],nums[nums[i]-1]);
+            }
         }
         for(int i=0;i<n;i++){
-            if(abs(nums[i])<n) nums[abs(nums[i])]=-abs(nums[abs(nums[i])]);
+            if(nums[i]!=i+1) return i+1;
         }
-        for(int i=1;i<n;i++){
-            if(nums[i]>0) return i;
-        }
-        return npresent?n+1:n;
+        return n+1;
     }
 };
