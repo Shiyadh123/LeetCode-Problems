29321221816f9cd6eb0c3a1d3a5eaa4551ec5a21
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-06-24 14:34:25 +0530

    Time: 2547 ms (5.01%), Space: 318.7 MB (5.01%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+bool isPossible(vector<vector<char>> board,int i,int j,char c){
+    for(int p=0;p<9;p++){
+        if(board[i][p]==c) return false;
+        if(board[p][j]==c) return false;
+    }
+    int colStart=(j/3)*3;
+    int rowStart=(i/3)*3;
+    for(int p=0;p<3;p++){
+            for(int q=0;q<3;q++){
+                if(board[rowStart+p][colStart+q]==c) return false;
+            }
+    }
+    return true;
+}
+    bool solve(vector<vector<char>>& board){
+         for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char c='1';c<='9';c++){
+                        if(isPossible(board,i,j,c)){
+                            board[i][j]=c;
+                            if(solve(board)) return true;
+                            board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+        return;
+    }
+};
