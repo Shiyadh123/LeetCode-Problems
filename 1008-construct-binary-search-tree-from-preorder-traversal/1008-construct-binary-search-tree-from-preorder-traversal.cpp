33769b72cac0a3e76110f4da9414081a92190d7e
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-07-04 10:34:17 +0530

    Time: 4 ms (83.11%), Space: 13.8 MB (47.87%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,31 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* createNode(vector<int>& preorder,int s,int e){
-      if(s>e) return NULL;
-      int rootVal=preorder[s];
-      TreeNode* newNode=new TreeNode(rootVal);
-      int lo=s+1,hi=e;
-      while(lo<=hi){
-        int mid=(lo+hi)/2;
-        if(preorder[mid]<rootVal) lo=mid+1;
-        else hi=mid-1;
-      }
-      newNode->left=createNode(preorder,s+1,hi);
-      newNode->right=createNode(preorder,hi+1,e);
-      return newNode;
+    TreeNode* createNode(vector<int>& preorder,int& i,int ub){
+      if(i>=preorder.size()||preorder[i]>=ub) return NULL;
+      TreeNode* newNode=new TreeNode(preorder[i]);
+        int currVal=preorder[i];
+      ++i;
+      newNode->left=createNode(preorder,i,currVal);
+      newNode->right=createNode(preorder,i,ub);
+        return newNode;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        return createNode(preorder,0,preorder.size()-1);
+        int i=0;
+        return createNode(preorder,i,10001);
     }
 };
