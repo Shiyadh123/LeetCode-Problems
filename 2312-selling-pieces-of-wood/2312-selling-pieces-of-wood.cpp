9e6541347b6068409c180aca1e1e9c48674ae3a3
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-07-05 20:06:04 +0530

    Time: 854 ms (32.38%), Space: 27.8 MB (44.86%) - LeetHub

--- 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    long long fun(int m, int n ,vector<vector<long long >> &pricesMap, vector<vector<long long >> &memo) {
-        if(memo[m][n]!=-1) return memo[m][n];
-        long long maxi=pricesMap[m][n];
-         for(int i=1;i<m;i++){
-            maxi=max(maxi,fun(i,n,pricesMap,memo)+fun(m-i,n,pricesMap,memo));
-        }
-         for(int i=1;i<n;i++){
-            maxi=max(maxi,fun(m,i,pricesMap,memo)+fun(m,n-i,pricesMap,memo));
-        }
-        memo[m][n]=maxi;
-        return maxi;
-    }
+    // long long fun(int m, int n ,vector<vector<long long >> &pricesMap, vector<vector<long long >> &memo) {
+    //     if(memo[m][n]!=-1) return memo[m][n];
+    //     long long maxi=pricesMap[m][n];
+    //      for(int i=1;i<m;i++){
+    //         maxi=max(maxi,fun(i,n,pricesMap,memo)+fun(m-i,n,pricesMap,memo));
+    //     }
+    //      for(int i=1;i<n;i++){
+    //         maxi=max(maxi,fun(m,i,pricesMap,memo)+fun(m,n-i,pricesMap,memo));
+    //     }
+    //     memo[m][n]=maxi;
+    //     return maxi;
+    // }
     long long sellingWood(int m, int n, vector<vector<int>>& prices) {
         vector<vector<long long >> pricesMap(m+1,vector<long long > (n+1,0));
-        vector<vector<long long >> memo(m+1,vector<long long > (n+1,-1));
+        vector<vector<long long >> dp(m+1,vector<long long > (n+1,0));
           for(int i=0;i<prices.size();i++){
              pricesMap[prices[i][0]][prices[i][1]]=prices[i][2];
         }
-        return fun(m,n,pricesMap,memo);
+         for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                long long maxi=pricesMap[i][j];
+                for(int k=1;k<i;k++){
+                    maxi=max(maxi,dp[k][j]+dp[i-k][j]);
+                }
+                for(int k=1;k<j;k++){
+                    maxi=max(maxi,dp[i][k]+dp[i][j-k]);
+                }
+                dp[i][j]=max(dp[i][j],maxi);
+            }
+        }
+        
+        return dp[m][n];
+        // return fun(m,n,pricesMap,memo);
     }
 };
