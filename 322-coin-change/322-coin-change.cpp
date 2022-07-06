3a37a7493a27bcc100bcbd64755158127e06b7c5
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-07-06 10:41:16 +0530

    Time: 203 ms (20.19%), Space: 49.4 MB (10.23%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,28 +2,20 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        
-        vector<vector<int>> dp(n,vector<int> (amount+1,1000001));
-        
-        for(int j=0;j<=amount;j++){
-            if(j%coins[0]==0) dp[0][j]=j/coins[0];
-            else dp[0][j]=1000001;
+        vector<vector<int>> dp(n+1,vector<int> (amount+1,0));
+        for(int i=0;i<=amount;i++){
+            dp[0][i]=10001;
         }
-        
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                
-                int notPick=dp[i-1][j];
-                
-                int pick=1000001;
-                if(coins[i]<=j){
-                    pick=1+dp[i][j-coins[i]];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                int notChoose=dp[i-1][j];
+                int choose=10001;
+                if(coins[i-1]<=j){
+                    choose=1+dp[i][j-coins[i-1]];
                 }
-                
-                dp[i][j]=min(pick,notPick);
+                dp[i][j]=min(choose,notChoose);
             }
         }
-        return (dp[n-1][amount]==1000001?-1:dp[n-1][amount]);
+        return dp[n][amount]==10001?-1:dp[n][amount];
     }
-    
 };
