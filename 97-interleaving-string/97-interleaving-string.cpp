c74330673641d0def4fce03ac8c2039138f7e783
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-07-07 13:17:59 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (43.01%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int l1,l2,l3;
     string ss1,ss2,ss3;
     bool fun(int i,int j,int k,vector<vector<int>> &memo){
-        if(i>=l1&&j>=l2&&k>=l3) return true;
-        if(memo[i][j]!=-1) return memo[i][j];
+        if(i<0&&j<0) return (k<0);
+        if(memo[i+1][j+1]!=-1) return memo[i+1][j+1];
         bool x=false,y=false;
-        if(i<l1&&ss1[i]==ss3[k]){
-            x=fun(i+1,j,k+1,memo);
+        if(i>=0&&ss1[i]==ss3[k]){
+            x=fun(i-1,j,k-1,memo);
         }
-        if(j<l2&&ss2[j]==ss3[k]){
-            y=fun(i,j+1,k+1,memo);
+        if(j>=0&&ss2[j]==ss3[k]){
+            y=fun(i,j-1,k-1,memo);
         }
-        memo[i][j]=x||y;
+        memo[i+1][j+1]=x||y;
         return x||y;
     }
     bool isInterleave(string s1, string s2, string s3) {
+        int l1,l2,l3;
         ss1=s1; l1=s1.length();
         ss2=s2; l2=s2.length();
         ss3=s3; l3=s3.length();
+        if(l1+l2!=l3) return false;
         vector<vector<int>> memo(l1+1,vector<int> (l2+1,-1));
-        return fun(0,0,0,memo);
+        return fun(l1-1,l2-1,l3-1,memo);
     }
 };
