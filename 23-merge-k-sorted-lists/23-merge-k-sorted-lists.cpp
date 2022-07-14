ffd93a63e5928ddf11c2ae5bfa1316062b6476b0
Muhammed Shiyadh <74063199+Shiyadh123@users.noreply.github.com>
2022-07-14 08:47:37 +0530

    Time: 20 ms (97.17%), Space: 12.9 MB (95.32%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -10,20 +10,43 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<pair<int,ListNode*>, vector<pair<int,ListNode*>>, greater<pair<int,ListNode*>> > pq;
-        for(auto i:lists){
-            if(i) pq.push({i->val,i});
+        ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!list1) return list2;
+        if(!list2) return list1;
+        ListNode* head=NULL,*temp=NULL,*p=list1,*q=list2;
+        while(p&&q){
+            if(p->val<q->val){
+                if(!head) head=p;
+                if(!temp) temp=p;
+                else {
+                    temp->next=p; temp=temp->next;
+                }
+                p=p->next;
+            }else{
+                if(!head) head=q;
+                if(!temp) temp=q;
+                else {
+                    temp->next=q; temp=temp->next;
+                }
+                q=q->next;
+            }
         }
-        ListNode* head=NULL;
-        ListNode* temp=NULL;
-        while(!pq.empty()){
-            pair<int,ListNode*> curr=pq.top();
-            pq.pop();
-            if(!head){ head=curr.second;temp=curr.second;}
-            else{ temp->next=curr.second; temp=temp->next;}
-            if(curr.second->next) pq.push({curr.second->next->val,curr.second->next});
+        if(!p){
+            temp->next=q;
+        }else{
+            temp->next=p;
         }
         return head;
+    }
+    ListNode* divide(vector<ListNode*>& lists,int l,int r){
+        if(l>=r) return lists[l];
+        int m=(l+r)/2;
+        ListNode* left=divide(lists,l,m);
+        ListNode* right=divide(lists,m+1,r);
+        return mergeTwoLists(left,right);
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0) return NULL;
+        return divide(lists,0,lists.size()-1);
     }
 };
